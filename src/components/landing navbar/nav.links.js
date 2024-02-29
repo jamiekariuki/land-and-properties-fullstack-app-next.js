1b1af422925a0fb127c8c1a160e9b40c0fced747
jamiekariuki <jamiekariuki18@gmail.com>
2024-02-29 21:18:12 +0300

    all components removed

--- src/components/landing navbar/nav.links.js
+++
@@ -1,7 +0,0 @@
-export const links = [
-	{ name: "About Us", href: "/about-us" },
-	{ name: "Gallery", href: "/gallery" },
-	{ name: "Properties", href: "/properties" },
-	{ name: "FAQ", href: "/faq" },
-	{ name: "Contact Us", href: "/contact-us" },
-];
