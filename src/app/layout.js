b797bc829dc93a4e84a438822c161f6655067206
jamiekariuki <jamiekariuki18@gmail.com>
2024-02-29 21:33:08 +0300

    middleware removed and commented on layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,13 @@
 import "./globals.scss";
 import { Cormorant, Bodoni_Moda } from "next/font/google";
-import { ThemeProvider2 } from "@/context/themeContext";
+//import { ThemeProvider2 } from "@/context/themeContext";
 import { cookies } from "next/headers";
-import Navbar from "@/components/landing navbar/navbar";
-import Sidebar from "@/components/landing navbar/sidebar/Sidebar";
-import Footer from "@/components/landing footer/footer";
+//import Navbar from "@/components/landing navbar/navbar";
+//import Sidebar from "@/components/landing navbar/sidebar/Sidebar";
+//import Footer from "@/components/landing footer/footer";
 
-import Toast from "@/components/styled components/toast/toast";
-import { ToastProvider } from "@/context/toastContext";
+//import Toast from "@/components/styled components/toast/toast";
+//import { ToastProvider } from "@/context/toastContext";
 import { currentServerUser } from "@/utils/get current user/get.current.server.user";
 
 //fonts
@@ -48,17 +48,15 @@ export default async function RootLayout({ children }) {
 	return (
 		<html lang="en" className={`${cormorant.variable} ${bodoni.variable} `}>
 			<body>
-				<ToastProvider>
-					<ThemeProvider2 serverDarkMode={darkMode}>
-						<Navbar user={user} />
-						<Sidebar />
-						<div className="pages">{children}</div>
-						<Footer />
-					</ThemeProvider2>
-					<div id="toast-portal">
-						<Toast />
-					</div>
-				</ToastProvider>
+				{/* 	<ToastProvider> */}
+				{/* <ThemeProvider2 serverDarkMode={darkMode}> */}
+				{/* <Navbar user={user} />
+						<Sidebar /> */}
+				<div className="pages">{children}</div>
+				{/* <Footer /> */}
+				{/* </ThemeProvider2> */}
+				<div id="toast-portal">{/* <Toast /> */}</div>
+				{/* </ToastProvider> */}
 			</body>
 		</html>
 	);
