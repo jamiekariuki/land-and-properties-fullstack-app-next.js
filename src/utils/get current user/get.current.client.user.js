d02cfc007862f680996b062c0ad284e233dbcf8c
jamiekariuki <jamiekariuki18@gmail.com>
2024-02-29 23:17:37 +0300

    use session commented

--- src/utils/get current user/get.current.client.user.js
+++ src/utils/get current user/get.current.client.user.js
@@ -1,7 +1,7 @@
-import { useSession } from "next-auth/react";
+/* import { useSession } from "next-auth/react";
 
 export const currentClientUser = () => {
 	const session = useSession();
 
 	return session.data?.user;
-};
+}; */
