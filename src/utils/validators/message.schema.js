2ef7baa4c8b8da2e6a15b1ecf5ebe04bda4a2a9a
jamiekariuki <jamiekariuki18@gmail.com>
2024-02-29 21:40:56 +0300

    utils removed

--- src/utils/validators/message.schema.js
+++
@@ -1,8 +0,0 @@
-import * as yup from "yup";
-
-export const MessageSchema = yup.object().shape({
-	name: yup.string(),
-	email: yup.string().email("Invalid email").required("Email is required"),
-	number: yup.string(),
-	message: yup.string().required("Message is required"),
-});
--- src/utils/validators/subscriber.schema.js
+++
@@ -1,5 +0,0 @@
-import * as yup from "yup";
-
-export const SubscriberSchema = yup.object().shape({
-	email: yup.string().email("Invalid email").required("Email is required"),
-});
