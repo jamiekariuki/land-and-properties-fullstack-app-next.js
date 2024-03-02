4cd76ce47abe1f380cc61a3acafa3c1df94d0f1b
jamiekariuki <jamiekariuki18@gmail.com>
2024-03-03 02:41:34 +0300

    fqa link removed

--- src/components/landing navbar/nav.links.js
+++ src/components/landing navbar/nav.links.js
@@ -2,6 +2,6 @@ export const links = [
 	{ name: "About Us", href: "/about-us" },
 	{ name: "Gallery", href: "/gallery" },
 	{ name: "Properties", href: "/properties" },
-	{ name: "FAQ", href: "/faq" },
+	/* { name: "FAQ", href: "/faq" }, */
 	{ name: "Contact Us", href: "/contact-us" },
 ];
--- src/components/landing pages/hero/background/backgound.jsx
+++ src/components/landing pages/hero/background/backgound.jsx
@@ -20,20 +20,6 @@ const Backgound = () => {
 		fade: true,
 	};
 
-	const keyStr =
-		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-
-	const triplet = (e1, e2, e3) =>
-		keyStr.charAt(e1 >> 2) +
-		keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
-		keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
-		keyStr.charAt(e3 & 63);
-
-	const rgbDataURL = (r, g, b) =>
-		`data:image/gif;base64,R0lGODlhAQABAPAA${
-			triplet(0, r, g) + triplet(b, 255, 255)
-		}/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`;
-
 	return (
 		<div className="bg-1">
 			<CardSlider settings={settings}>
@@ -44,8 +30,6 @@ const Backgound = () => {
 							alt="background photo"
 							fill={true}
 							className="bg-image"
-							placeholder="blur"
-							blurDataURL={rgbDataURL(255, 255, 255)}
 						/>
 					</div>
 				))}
